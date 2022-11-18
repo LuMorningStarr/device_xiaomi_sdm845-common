8934753e2acca829c93dc3781c1da2743c089fab
LucyfMorning <unreadmatheus@gmail.com>
2022-11-18 05:11:03 +0000

    Revert "sdm845-common: switch to retrofit dynamic partitioning"
    This reverts commit 896b1bbb8b74165654eb6bf95705a6d41a2a5628.

--- sdm845.mk
+++ sdm845.mk
@@ -320,13 +320,6 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.2 \
     android.hardware.neuralnetworks@1.2.vendor
 
-# Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/partitions/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
-
 # Perfd (dummy)
 PRODUCT_PACKAGES += \
     libqti-perfd-client
